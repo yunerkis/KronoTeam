6f6d01ccd3bafc8a3df3844e5fc975693aac45b9
yunerkis <yunerkis.leal@gmail.com>
2018-04-20 19:24:14 -0500

    add categories

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Filter } from '../filter.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  categories = []
+
+  constructor(public storeService: Filter) {
+    this.categories = storeService.getCategories();
+  }
 
   ngOnInit() {
   }
